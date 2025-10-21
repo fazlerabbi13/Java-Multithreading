f4d5318af5b7fa9fe549af00c9d893d498ff9a92
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-21 20:19:53 +0600

    start thread using start method().

--- Synchronization/Main.java
+++ Synchronization/Main.java
@@ -22,5 +22,21 @@ public class Main {
                 cnt.increment();
             }
         });
+
+        Thread t2 = new Thread(()->{
+            for(int i = 0; i < 1000; i++){
+                cnt.increment();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try{
+            t1.join();
+            t2.join();
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
     }
 }
