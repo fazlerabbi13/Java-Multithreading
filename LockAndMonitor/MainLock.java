e187249704c259c1d7b94b05f6b68928a5ad98d3
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-22 19:27:25 +0600

    create two thread to increment the shared resourse.

--- LockAndMonitor/MainLock.java
+++ LockAndMonitor/MainLock.java
@@ -7,10 +7,21 @@ public class MainLock {
     private static final ReentrantLock lock = new ReentrantLock();
 
     public static void main(String[] args) {
-        
+         Thread t1 = new Thread(new IncrementTask());
+         Thread t2 = new Thread(new IncrementTask());
+
+         t1.start();
+         t2.start();
+
+         try{
+            t1.join();
+            t2.join();
+         }catch(InterruptedException e){
+            System.out.println("Thread inturrupted");
+         }
     }
 
-    static class IncrementTask implements Readable{
+    static class IncrementTask implements Runnable{
         public void run(){
             for(int i = 0; i < 1000; i++){
                 lock.lock();
